c884a8f0ea514eb05665e6442691d0bab1991a0d
Dennis N <dnickell@protonmail.com>
2021-07-25 11:11:21 -0500

    initial set up, start js

--- js/site.js
+++ js/site.js
@@ -1,5 +1,31 @@
 
-//Call Hello World
-function helloWorld() {
-    alert("hello world");
+//1.get values from the interface
+//start controller function
+
+function getValues () {
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
+
+//3.validate input / parse into integers
+
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
+//5.
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+    numbers = generateNumbers(fizzValue, buzzValue);
+
+    } else {
+    alert("You must enter a number!");
+}
+    let fbdata = FizzBuzz(fizzValue, buzzValue);
+}
+
+//logic functions
+//generate numbers from the fizzValue to the buzzValue
+
+function generateNumbers(fizzValue, buzzValue) {
+
+    let numbers = [];
+    
+
 }
